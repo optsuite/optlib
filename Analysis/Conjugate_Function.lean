706c21cd561e007e568f04b627d384305a22bbbd
cyl <108876608+chenyili0818@users.noreply.github.com>
2023-10-28 20:16:54 +0800

    Update Conjugate_Function.lean
    the definition needs to be discussed

--- Analysis/Conjugate_Function.lean
+++ Analysis/Conjugate_Function.lean
@@ -1,39 +1,80 @@
 /-
 Copyright (c) 2023 . All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors:
+Authors: Chenyi Li
 -/
 import Mathlib.Analysis.Convex.Basic
 import Mathlib.Data.Real.EReal
-import Mathlib.Order.Bounds.Basic
-import Mathlib.Analysis.NormedSpace.PiLp
-import Mathlib.LinearAlgebra.FiniteDimensional
-import Mathlib.LinearAlgebra.UnitaryGroup
-import Mathlib.Analysis.InnerProductSpace.Projection
 import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Topology.Semicontinuous
+
 /-!
   the defition of the conjugate function and the Fenchel inequality
   need to solve the sorry
 -/
-open LinearMap Set BigOperators Classical Convex Pointwise
-
 noncomputable section
 
 open InnerProductSpace
 
-variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E] 
-variable {f : E → ℝ} 
+variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E]
+variable {f : E → ℝ} {x y : E}
 
-def cg (f : E → ℝ) (y : E) : ℝ :=
-  sSup {@inner ℝ _ _ y x - f x | x : E}
+/-
+  the definition of the conjugate function 
+-/
+def cg (f : E → ℝ) (y : E) : EReal :=
+  sSup {(inner y x : ℝ) - f x | x : E}
 
---Fenchel 不等式
-theorem Fenchel (f : E → ℝ) (y x: E) : f x + cg f y  ≥  @inner ℝ _ _ y x := by
+/-
+  the domain of the conjugate function (the set where the infty is not attained)
+-/
+def cg_dom (f : E → ℝ) : Set E :=
+  {x : E | (sSup {(inner x y : ℝ) - f x | y : E} : EReal) ≠ ⊤}
+
+/-
+  the strict conjugate function where the image set is ℝ 
+-/
+def strict_cg (f : E → ℝ) (y : E) : ℝ :=
+  sSup {inner y x - f x | x : E}
+
+section
+
+/- Fenchel inequality -/
+theorem Fenchel (f : E → ℝ) (y x: E) : cg f y ≥ ((inner y x) : ℝ) - f x:= by
   rw [cg]
-  have h1 : sSup {x | ∃ x_1, inner y x_1 - f x_1 = x} ≥ inner y x - f x := by
-    sorry
-  exact Iff.mp tsub_le_iff_left h1
+  apply le_sSup
+  use x
 
-lemma quadratic {b : E} {c : ℝ} :
-    cg (fun x ↦ 1 / 2 * inner x x + inner b x + c) y = 1 / 2 * inner (y - b) (y - b) - c := by
+theorem convex_cg (f : E → ℝ) (y : E) : ConvexOn ℝ (cg_dom f) (fun x => strict_cg f x) := by
+  sorry
+
+lemma quadratic_cg {b : E} {c : ℝ} :
+    strict_cg (fun x ↦ 1 / 2 * inner x x + inner b x + c) y = (inner (y - b) (y - b) / 2 - c) := by
+  sorry
+
+lemma norm_cg_dom : cg_dom (fun x ↦ ‖x‖) = {x : E | ‖x‖ ≤ 1} := by
+  sorry
+
+lemma norm_cg_value : ∀ (x : E), x ∈ cg_dom (fun x ↦ ‖x‖) → strict_cg (fun t ↦ ‖t‖) x = 0 := by
+  sorry
+
+/-
+  the definition of the conjugate function for the conjugate function of a function
+-/
+
+def cg_double (f : E → ℝ) (y : E) : ℝ :=
+  sSup {(inner y x : ℝ) - cg f x | x ∈ cg_dom f}
+
+theorem Fenchel_double (f : E → ℝ) : cg_double f x ≤ f x := by
+  sorry
+
+theorem convex_cg_double (f : E → ℝ) (y : E) : ConvexOn ℝ (cg_dom f) (fun x => cg_double f x) := by
+  sorry
+
+theorem close_cg_double (f : E → ℝ) (y : E) :
+    LowerSemicontinuousOn (fun x => cg_double f x) (cg_dom f) := by
+  sorry
+
+theorem convex_close_cg_couble (f : E → ℝ) (hfun : ConvexOn ℝ Set.univ f)
+    (hc : LowerSemicontinuousOn f Set.univ) : f x = cg_double f x := by
   sorry

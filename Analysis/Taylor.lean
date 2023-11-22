b9567386b3646b98034f4f9bafdd710fc6a90850
cyl <2000010865@stu.pku.edu.cn>
2023-11-22 17:06:17 +0800

    Update Taylor.lean

--- Analysis/Taylor.lean
+++ Analysis/Taylor.lean
@@ -17,6 +17,20 @@ theorem expansion (h : ∀ x : E, HasGradientAt f (f' x) x) (x p : E) :
     ∃ t, t > 0 ∧ t < 1 ∧ f (x + p) = f x + inner (f' (x + t • p)) p := by
   sorry
 
-theorem continuous (h : ContinuousAt f x) : ∀ ε > 0, ∃ δ > 0, ∀ (y : E), ‖y - x‖ < δ → |f y - f x| < ε := by
-  rw [ContinuousAt] at h
-  sorry
+-- This theorem should not place here, but let's just first do this for now.
+theorem continuous (h : ContinuousAt f x) : ∀ ε > 0, ∃ δ > 0, ∀ (y : E), ‖y - x‖ < δ
+    → ‖f y - f x‖ < ε := by
+  rw [continuousAt_def] at h
+  intro ε epos
+  let A := Metric.ball (f x) ε
+  specialize h A (Metric.ball_mem_nhds (f x) epos)
+  rw [Metric.mem_nhds_iff] at h
+  rcases h with ⟨δ, dpos, h⟩
+  use (δ / 2); constructor
+  exact half_pos dpos
+  intro x' x1le
+  have H1: x' ∈ Metric.ball x δ := by
+    rw [Metric.ball, Set.mem_setOf, dist_comm, dist_eq_norm_sub, norm_sub_rev]
+    apply lt_trans x1le
+    linarith
+  exact h H1

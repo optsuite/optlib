807f722820757e674cb62c7e7c347f894baa6ca9
cyl <2000010865@stu.pku.edu.cn>
2023-11-29 16:56:29 +0800

    Update

--- Function/StonglyPlus.lean
+++ Function/StonglyPlus.lean
@@ -6,10 +6,13 @@ Authors: Chenyi Li, Ziyu Wang
 import Mathlib.Analysis.InnerProductSpace.PiL2
 import Mathlib.Analysis.Convex.Strong
 import Analysis.Calculation
+import Function.Lsmooth
 
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E]
 
-variable (s : Set E) {f : E → ℝ} {m : ℝ} {xm xm': E} {f' : E → E}
+section Strongly_Convex
+
+variable {s : Set E} {f : E → ℝ} {m : ℝ} {xm xm': E} {f' : E → E}
 
 theorem Strongly_Convex_Bound (m : ℝ) (strongly_convex: StrongConvexOn s m f):
   ∀ ⦃x⦄, x ∈ s → ∀ ⦃y⦄, y ∈ s →
@@ -48,3 +51,26 @@ theorem Strong_Convex_second_lower (hsc: StrongConvexOn s m f)
     (hf : ∀ x ∈ s, HasGradientAt f (f' x) x) : ∀ x ∈ s, ∀ y ∈ s,
     f y ≥ f x + inner (f' x) (y - x) + m / 2 * ‖y - x‖ ^ 2 := by
   sorry
+
+end Strongly_Convex
+
+section Strongly_Convex_Gradient_Descent
+
+variable {f : E → ℝ} {l m : ℝ} {f' : E → E} {xm x₀: E} {point : ℕ → E}
+
+open Set
+
+theorem Strong_convex_Lipschitz_smooth (hsc: StrongConvexOn univ m f)
+    (hf : ∀ x, HasGradientAt f (f' x) x) (h₂ : LipschitzOn f' l) (hl : l > 0):
+    inner (f' x - f' y) (x - y) ≥ m * l / (m + l) * ‖x - y‖ ^ 2
+      + 1 / (m + l) * ‖f' x - f' y‖ ^ 2 := by
+  sorry
+
+lemma gradient_method_strong_convex (hsc: StrongConvexOn univ m f)
+    (hf : ∀ x₁ : E, HasGradientAt f (f' x₁) x₁) (h₃ : LipschitzOn f' l) (hl: l > 0)
+    (step₁: a ≤ 2 / (m + l)) (step₂ : a > 0) (initial : point 0 = x₀)
+    (update: ∀ (k : ℕ), point (k + 1) = point k - a • (f' (point k))):
+    ∀ k : ℕ , ‖point k - xm‖ ^ 2 ≤ (1 - a * (2 * m * l / (m + l))) ^ k * ‖x₀ - xm‖ ^ 2 := by
+  sorry
+
+end Strongly_Convex_Gradient_Descent

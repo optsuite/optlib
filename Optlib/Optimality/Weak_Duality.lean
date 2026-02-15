106556aa3eb0c447fc80dcee4c86825c04817eac
chenyili0818 <lichenyi@stu.pku.edu.cn>
2026-02-15 21:11:23 +0800

    add: update contributors

--- Optlib/Optimality/Weak_Duality.lean
+++ Optlib/Optimality/Weak_Duality.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 . All rights reserved.
+Copyright (c) 2025 Chenyi Li, Zaiwen Wen. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author:
+Author: Chenyi Li, Zaiwen Wen
 -/
 import Optlib.Optimality.Constrained_Problem
 import Optlib.Convex.ConvexFunction
